cea15ef531af07d7aa9809f2260dd0da10a98a89
Zhendong <690539306@qq.com>
2018-08-01 00:19:09 +0800

    控制台飞行棋项目，游戏头，地图初始化

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlyingChess
+{
+    class Program
+    {
+        public static int[] Maps = new int[100];
+
+        static void Main(string[] args)
+        {
+            GameShow();
+            InitialMap();
+            Console.ReadKey();
+        }
+
+        public static void GameShow()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("**************************");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("**************************");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("**************************");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("**************************");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("***飞行棋---FlyingChess***");
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("**************************");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("**************************");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("**************************");
+        }
+
+        public static void InitialMap()
+        {
+            int[] luckyturn = { 6, 23, 40, 55, 69, 83 };//幸运轮盘
+            for (int i = 0; i < luckyturn.Length; i++)
+            {
+                Maps[luckyturn[i]] = 1;
+            }
+
+            int[] landMine = { 5, 13, 17, 33, 38, 50, 64, 80, 94 };//地雷
+            for (int i = 0; i < landMine.Length; i++)
+            {
+                Maps[landMine[i]] = 2;
+            }
+
+            int[] pause = { 9, 27, 60, 93 };//暂停
+            for (int i = 0; i < pause.Length; i++)
+            {
+                Maps[pause[i]] = 3;
+            }
+
+            int[] timeTunnel = { 20, 25, 45, 63, 72, 88, 90 };//时空隧道
+            for (int i = 0; i < timeTunnel.Length; i++)
+            {
+                Maps[timeTunnel[i]] = 4;
+            }
+        }
+    }
+}
