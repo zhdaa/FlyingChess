bd5aa01263ac815d25be4041926af553e54e8dd2
Zhendong <690539306@qq.com>
2018-08-02 00:34:23 +0800

    Draw Map

--- Program.cs
+++ Program.cs
@@ -8,15 +8,23 @@ namespace FlyingChess
 {
     class Program
     {
-        public static int[] Maps = new int[100];
+        //用静态字段模拟全局变量
+        static int[] Maps = new int[100];
+
+        //声明一个静态数组来存储玩家A跟玩家B的坐标
+        static int[] PlayerPos = new int[2];
 
         static void Main(string[] args)
         {
             GameShow();
             InitialMap();
+            DrawMap();
             Console.ReadKey();
         }
 
+        /// <summary>
+        /// 画游戏头
+        /// </summary>
         public static void GameShow()
         {
             Console.ForegroundColor = ConsoleColor.Blue;
@@ -37,31 +45,135 @@ namespace FlyingChess
             Console.WriteLine("**************************");
         }
 
+        /// <summary>
+        /// 初始化地图
+        /// </summary>
         public static void InitialMap()
         {
-            int[] luckyturn = { 6, 23, 40, 55, 69, 83 };//幸运轮盘
+            int[] luckyturn = { 6, 23, 40, 55, 69, 83 };//幸运轮盘◎
             for (int i = 0; i < luckyturn.Length; i++)
             {
                 Maps[luckyturn[i]] = 1;
             }
 
-            int[] landMine = { 5, 13, 17, 33, 38, 50, 64, 80, 94 };//地雷
+            int[] landMine = { 5, 13, 17, 33, 38, 50, 64, 80, 94 };//地雷☆
             for (int i = 0; i < landMine.Length; i++)
             {
                 Maps[landMine[i]] = 2;
             }
 
-            int[] pause = { 9, 27, 60, 93 };//暂停
+            int[] pause = { 9, 27, 60, 93 };//暂停▲
             for (int i = 0; i < pause.Length; i++)
             {
                 Maps[pause[i]] = 3;
             }
 
-            int[] timeTunnel = { 20, 25, 45, 63, 72, 88, 90 };//时空隧道
+            int[] timeTunnel = { 20, 25, 45, 63, 72, 88, 90 };//时空隧道卍
             for (int i = 0; i < timeTunnel.Length; i++)
             {
                 Maps[timeTunnel[i]] = 4;
             }
+        }
+
+        /// <summary>
+        /// 画地图
+        /// </summary>
+        public static void DrawMap()
+        {
+            #region 第一横行
+            for (int i = 0; i < 30; i++)
+            {
+                Console.Write(DrawStringMap(i));
+            }
+            //画完第一行后，应该换行
+            Console.WriteLine();
+            #endregion
+
+            #region 第一竖行
+            for (int i = 30; i < 35; i++)
+            {
+                for (int j = 0; j <= 28; j++)
+                {
+                    Console.Write("  ");
+                }
+                Console.Write(DrawStringMap(i));
+                Console.WriteLine();
+            }
+            #endregion
+
+            #region 第二横行
+            for (int i = 64; i >= 35; i--)
+            {
+                Console.Write(DrawStringMap(i));
+            }
+            Console.WriteLine();
+            #endregion
+
+            #region 第二竖行
+            for (int i = 65; i < 70; i++)
+            {
+                Console.WriteLine(DrawStringMap(i));
+            }
+            #endregion
+
+            #region 第三横行
+            for (int i = 70; i <= 99; i++)
+            {
+                Console.Write(DrawStringMap(i));
+            }
+            #endregion
+        }
+
+        /// <summary>
+        /// 从画地图的方法中抽象出来的一个方法
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        public static string DrawStringMap(int i)
+        {
+            string str = "";
+            #region 画图
+            //如果玩家A跟玩家B的坐标相同，并且都在这个地图上，画一个尖括号
+            if (PlayerPos[0] == PlayerPos[1] && PlayerPos[0] == i)
+            {
+                str = "<>";
+            }
+            else if (PlayerPos[0] == i)
+            {
+                str = "Ａ";
+            }
+            else if (PlayerPos[1] == i)
+            {
+                str = "Ｂ";
+            }
+            else
+            {
+                switch (Maps[i])
+                {
+                    case 0:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        str = "□";
+                        break;
+                    case 1:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        str = "◎";
+                        break;
+                    case 2:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        str = "☆";
+                        break;
+                    case 3:
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        str = "▲";
+                        break;
+                    case 4:
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                        str = "卍";
+                        break;
+                }
+            }
+            return str;
+            #endregion
         }
     }
 }
